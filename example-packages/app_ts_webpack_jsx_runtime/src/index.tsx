c583213102fce78d3a459541f8eb3d0e1493e637
yahiro <yahiro07@users.noreply.github.com>
2021-11-14 06:38:37 +0900

    add development code

--- example-packages/app_ts_webpack_jsx_runtime/src/index.tsx
+++ example-packages/app_ts_webpack_jsx_runtime/src/index.tsx
@@ -1,14 +1,27 @@
-import { render, rerender } from 'alumina';
+import { css, FC, QxNode, render, rerender } from 'alumina';
 
 let message = 'hello';
 
-const AppRoot = () => {
-  return <div>hello {message}</div>;
+const BoxFrame: FC<{ children: QxNode }> = ({ children }) => {
+  const style = css`
+    border: solid 1px blue;
+  `;
+  return <div class={style}>{children}</div>;
+};
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>app ts webpack jsx runtime</h1>
+      <div>hello {message}</div>
+      <BoxFrame>foo</BoxFrame>
+    </div>
+  );
 };
 
 window.addEventListener('load', () => {
   console.log('start');
-  render(AppRoot, document.getElementById('app'));
+  render(() => <App />, document.getElementById('app'));
   setTimeout(() => {
     message = 'world';
     rerender();
