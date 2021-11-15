df01ef0ddca32791cefb868b00675b4c730e6b0a
yahiro <yahiro07@users.noreply.github.com>
2021-11-15 10:06:15 +0900

    add example code

--- example-packages/app_ts_webpack/src/index.tsx
+++ example-packages/app_ts_webpack/src/index.tsx
@@ -1,4 +1,5 @@
-import { render, jsx, rerender, Fragment, useEffect } from 'alumina';
+/* eslint-disable @typescript-eslint/unbound-method */
+import { render, jsx, rerender, useEffect, css } from 'alumina';
 
 namespace ns0 {
   let message = 'hello';
@@ -12,21 +13,77 @@ namespace ns0 {
 
   export const App = () => {
     useEffect(updateMessageAsync, []);
+    return <div>hello {message}</div>;
+  };
+}
+
+namespace ns1 {
+  let text = '⭐';
+
+  export const Dolphins = () => (
+    <div onClick={() => (text += '🐬')}>{text}</div>
+  );
+}
+
+namespace ns2 {
+  class WhalesModel {
+    text = '⭐';
+    extend = () => (this.text += '🐳');
+  }
+  const model = new WhalesModel();
+
+  export const Whales = () => {
+    const { text, extend } = model;
+    return <div onClick={extend}>{text}</div>;
+  };
+}
+
+namespace ns3 {
+  const state = {
+    text: '⭐',
+  };
+  const getters = {
+    get whalesDouble() {
+      return state.text.replace(/🐬/g, '🐳🐳');
+    },
+  };
+  const actions = {
+    extend() {
+      state.text += '🐬';
+    },
+    reset() {
+      state.text = '⭐';
+    },
+  };
+
+  export const App = () => {
+    const { text } = state;
+    const { whalesDouble } = getters;
+    const { extend, reset } = actions;
     return (
       <div>
-        hello {message}
-        <>
-          <p>123</p>
-        </>
+        <div>dolphins: {text}</div>
+        <div>whales_x2: {whalesDouble}</div>
+        <div>
+          <button onClick={extend}>add</button>
+          <button onClick={reset}>reset</button>
+        </div>
       </div>
     );
   };
 }
 
 const AppRoot = () => {
+  const style = css`
+    font-size: 30px;
+    user-select: none;
+  `;
   return (
-    <div>
+    <div class={style}>
       <ns0.App />
+      <ns1.Dolphins />
+      <ns2.Whales />
+      <ns3.App />
     </div>
   );
 };
