96ed41863da64e92a1ea52d5ca239fd90b0cd2a9
yahiro <yahiro07@users.noreply.github.com>
2022-01-04 22:01:06 +0900

    update example

--- example-packages/app-ts-vite/src/index.tsx
+++ example-packages/app-ts-vite/src/index.tsx
@@ -1,11 +1,39 @@
-import { css, jsx, render } from "alumina";
+import { css, FC, jsx, render } from 'alumina';
 
 let count = 0;
-const App = () => (
+const Counter: FC = () => (
   <div class={style} onClick={() => count++}>
     hello world {count}
   </div>
 );
+
+const TitleWithButton: FC<{ text: string; handler?: () => void }> = ({
+  text,
+  handler,
+}) => (
+  <div style="display:flex">
+    <div>{text}</div>
+    <button onClick={handler} if={handler}>
+      callback
+    </button>
+  </div>
+);
+
+const user = {
+  name: 'yamada',
+  age: 20,
+};
+
+const App: FC = () => {
+  return (
+    <div>
+      <Counter />
+      <div if={user}>{user.name}</div>
+      <TitleWithButton text="foo" />
+      <TitleWithButton text="bar" handler={() => alert('hello')} />
+    </div>
+  );
+};
 
 const style = css`
   border: solid 2px #f08;
@@ -13,6 +41,6 @@ const style = css`
   user-select: none;
 `;
 
-window.addEventListener("load", () => {
-  render(() => <App />, document.getElementById("app"));
+window.addEventListener('load', () => {
+  render(() => <App />, document.getElementById('app'));
 });
